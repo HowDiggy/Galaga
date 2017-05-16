134f1f26895b4282bd8019f29d53e2d363fd8b68
Paulo Jauregui <jauregui.paulo@gmail.com>
2017-05-16 11:06:12 -0700

    Testing upload

---
+++ asteroid.cpp
@@ -0,0 +1,44 @@
+#include "asteroid.hpp"
+#include <unistd.h>
+
+#define DELAY 200000 // DELAY directly correlates with any movement of objects in the window the bigger the DELAY the slower an object moves.
+
+Asteroid::Asteroid(){
+
+  unsigned int x=1;
+  int y=0;
+  int xMax= 50; // will change this value to an variable
+  int xMin0 = 0;
+  int yMax = 100;
+}
+
+void Asteroid::setX(){
+  srand(time(NULL));
+  int x1 = 1 + (rand() % 50); // will change this value to an variable
+  x = x1;
+}
+
+
+void Asteroid::setY(){
+  y++;
+}
+
+
+int Asteroid::getY(){
+return y;
+}
+int Asteroid::getX(){
+return x;
+}
+
+void Asteroid::printAsteroid(){
+
+    // previously-printed characters
+    mvprintw(y, x + 1 , "o"); // Print our "ball" at the current xy position
+    refresh();
+    mvprintw(y + 1, x, "ooo");
+    refresh();
+    mvprintw(y + 2, x + 1, "o");
+    refresh();
+    usleep(DELAY); // Shorter delay between movements
+}
---
+++ asteroid.hpp
@@ -0,0 +1,29 @@
+//asteroid.h
+
+
+#include <ncurses.h>
+#include <unistd.h>
+#include <ctime>
+#include <vector>
+#include <cstdlib>
+
+
+
+class Asteroid{
+private:
+  int x, y, xMax, xMin, yMax;
+  char character;
+
+public:
+  Asteroid();
+  void setY();
+  void setX();
+  int getY();
+  int getX();
+
+
+  void printAsteroid();
+
+
+
+};
