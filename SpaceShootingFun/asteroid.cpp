4c5c24f13ad2c1aa3a7afc99269381261580530a
Paulo <jauregui.paulo@gmail.com>
2017-05-18 12:57:50 -0700

    updated main and asteroid files.

--- SpaceShootingFun/asteroid.cpp
+++ SpaceShootingFun/asteroid.cpp
@@ -5,26 +5,24 @@
 
 Asteroid::Asteroid(){
 
-  unsigned int x=1;
-  int y=0;
-  int xMax= 50; // will change this value to an variable
-  int xMin0 = 0;
-  int yMax = 100;
+  int y = 0;
+  int max_y = 0, max_x = 0;
+  int next_y = 0, next_x = 0;
+  int direction_y = 1;
+  int direction_x = 1;
+  int waveCount = 1;
 }
 
 void Asteroid::setX(){
 
-  int x1 = 1 + (rand() % 50); // will change this value to an variable
-  x = x1;
 }
 
 
 void Asteroid::setY(){
-  y++;
 }
 
 void Asteroid::modY(int i){
-  y = i;
+
 }
 int Asteroid::getY(){
   return y;
@@ -35,15 +33,50 @@ int Asteroid::getX(){
 
 void Asteroid::printAsteroid(){
 
-    // previously-printed characters
-    mvprintw(y, x + 1 , "o"); // Print our "ball" at the current xy position
-    refresh();
-    mvprintw(y + 1, x, "ooo");
-    refresh();
-    mvprintw(y + 2, x + 1, "o");
-    refresh();
-    //usleep(DELAY); // Shorter delay between movements
-    setY();
+  // Global var `stdscr` is created by the call to `initscr()`
+  //getmaxyx(stdscr, max_y, max_x);
+
+  std::default_random_engine generator;
+
+
+  for(int i=0; i < waveCount; i++){
+
+
+
+   while(1) {
+
+     getmaxyx(stdscr, max_y, max_x);
+
+     std::uniform_int_distribution<int> distribution(1,max_x);
+     int x = distribution(generator);  // generates number in the range 1..6
+   // next_x = x + direction_x;
+   // x+= direction_x;
+
+     while(next_y != max_y) {
+         clear();
+         mvprintw(y, x, " /\\ ");
+         mvprintw(y+1, x, " \\/ ");
+         //mvprintw(y+2, x, " o ");
+         refresh();
+
+         usleep(DELAY);
+
+         next_y = y + direction_y;
+         y+= direction_y;
+
+
+         }
+
+     y = 1;
+     next_y = 0;
+     }
+   // x = 1;
+   // next_x = 0;
+  }
+
+
+
+
 }
 
 // void Asteroid::moveAsteroid(){
--- SpaceShootingFun/main.cpp
+++ SpaceShootingFun/main.cpp
@@ -7,6 +7,7 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <ctime>
+#include <random>
 <<<<<<< Updated upstream
 #include <vector>
 =======
@@ -24,28 +25,28 @@ int main(int argc, char ** argv)
     initscr(); //initialize screen-always include/sets up memory and clears screen
     cbreak(); //Control+C exits program
     noecho(); //user input doesn't show on screen
-    
+
     //get screen size
     int yMax, xMax;
     getmaxyx(stdscr, yMax, xMax);
-    
+
     //set size of window-position on terminal screen
     int height, width, start_y, start_x;
     height = 45;
     width = 80;
     start_y = 0;
     start_x = 0; //added value moves window right
-    
+
     //customize window appearance-using pointer
     WINDOW * win = newwin(height, width, start_y, start_x);
     refresh();
-    
+
     char plus = '+';
     char dash = '-';
     char line = '|';
-    
+
     int left, right, top, bottom, tlc, trc, blc, brc;
-    
+
     left = line;
     right = line;
     top = dash;
@@ -54,149 +55,39 @@ int main(int argc, char ** argv)
     brc = plus;
     tlc = plus;
     trc = plus;
-    
+
     wborder(win, left, right, top, bottom, tlc, trc, blc, brc);
     //wborder(win, left, right, top, bottom, tlc, trc, blc, brc); accepts int values or characters
-    
+
     WINDOW * win2 = newwin(4, 80, 45, 0); //height, width, start_y, start_x)
-    
+
     wborder(win2, left, right, top, bottom, tlc, trc, blc, brc);
-    
+
     mvwprintw(win2, 1, 1, "Count: %i"); //wave-checkpoint mark
-    
+
     mvwprintw(win2, 2, 1, "Use UP, DOWN, LEFT, RIGHT arrow keys. CTRL+C to escape.");
-    
+
     wrefresh(win2);
-    
+
     //--------------------------------Asteroid-------------------------------
-    
-    srand(time(NULL));
-    
-    //collision
-    //int direction = 1;
-    //int next_x = 0;
-    
-    int x = 0, y = 0;
-    int numberOfAsteroids = 3;
-    
-    vector <Asteroid> v; //vector of Asteroids
-    
-    v.resize(numberOfAsteroids);
-    
-    while(1)
-    {
-        getmaxyx(stdscr, yMax, xMax);
-        clear();
-        
-        for(int i = 0; i < numberOfAsteroids; i++)
-        {
-            v[i].setX();
-            v[i].modY(1);
-            v[i].printAsteroid();
-        }
-    
-    //refresh();
-    
-        for(int j = 0; j < 40; j++)
-        {
-            for(int i = 0; i < numberOfAsteroids; i++)
-            {
-                v[i].modY(v[i].getY() + 1);
-            }
-        
-            clear();
-            
-        
-            for(int i = 0; i < numberOfAsteroids; i++)
-            {
-                v[i].printAsteroid();
-            
-                /*
-                 next_x = y + direction; //not x
-            
-                 if (next_x >= v[i].yMax || next_x < 0)
-                 {
-                    direction*= -1;
-                 }
-                 else
-                 {
-                    y+= direction; //not x
-                 }*/
-            }
-     
-        usleep(150000); //200000
-     
-        //refresh();
-        }
-    }
-    
+
+    printAsteroid();
     //--------------------------------------------------------------------------
 
     //pointer to Player
     Ship * p = new Ship(win, 41, 39, '^', '(', ')', '0', '[', ']', '[', ']');
-    
+
     //Asteroid * a = new Asteroid(win, y, x);
     vector <Asteroid> *a = new vector <Asteroid>(win, y, x);
-    
+
      //vector <Asteroid> a[3]; //vector of Asteroids
     //a->resize(3);
-   
-    
+
+
     while(p->getmv() != 'x')
     {
         p->display();
         refresh();
-        
-
-=======
- int main(int argc, char ** argv)
- {
-     /*NCurses START*/
-     initscr(); //initialize screen-always include/sets up memory and clears screen
-     cbreak(); //Control+C exits program
-     noecho(); //user input doesn't show on screen
-
-     //get screen size
-     int yMax, xMax;
-     getmaxyx(stdscr, yMax, xMax);
-
-     //set size of window-position on terminal screen
-     int height, width, start_y, start_x;
-     height = 45;
-     width = 80;
-     start_y = 0;
-     start_x = 0; //added value moves window right
-
-     //customize window appearance-using pointer
-     WINDOW * win = newwin(height, width, start_y, start_x);
-     refresh();
-
-     char plus = '+';
-     char dash = '-';
-     char line = '|';
-
-     int left, right, top, bottom, tlc, trc, blc, brc;
-
-     left = line;
-     right = line;
-     top = dash;
-     bottom = dash;
-     blc = plus;
-     brc = plus;
-     tlc = plus;
-     trc = plus;
-
-     wborder(win, left, right, top, bottom, tlc, trc, blc, brc);
-     //wborder(win, left, right, top, bottom, tlc, trc, blc, brc); accepts int values or characters
-
-     WINDOW * win2 = newwin(4, 80, 45, 0);
-
-     wborder(win2, left, right, top, bottom, tlc, trc, blc, brc);
-
-     mvwprintw(win2, 1, 1, "Count: %i"); //wave-checkpoint mark
-
-     mvwprintw(win2, 2, 1, "Use UP, DOWN, LEFT, RIGHT arrow keys. CTRL+C to escape.");
->>>>>>> Stashed changes
 
     }
 
@@ -211,34 +102,13 @@ int main(int argc, char ** argv)
         mvwprintw(score_win, 0, 0, "Score: %d", score);
         wrefresh(score_win);
     }*/
-    
+
     //make sure program waits before exiting
     getch();
-    
+
     //deallocates memory and ends ncurses
     endwin();
-    
+
     /*NCurses END*/
     return 0;
 }
-=======
-     //pointer to Player
-     Player * p = new Player(win, 43, 39, '@'); //pointer to Player class, with win parameters
-
-     while(p->getmv() != 'x')
-     {
-         p->display();
-
-         refresh();
-     }
-
-     //make sure program waits before exiting
-     getch();
-
-     //deallocates memory and ends ncurses
-     endwin();
-
-     /*NCurses END*/
- return 0;
- }
->>>>>>> Stashed changes
